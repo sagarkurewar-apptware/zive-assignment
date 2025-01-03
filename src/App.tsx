f88e9e01ea7201ff6995d728102a02f0b6ea5daa
Sagar <sagar.kurewar@apptware.com>
2025-01-03 16:15:51 +0530

    created new react app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="text-red-600 font-bold text-[2rem] ">Hello, Welcome to New React App with TS</h1>
+    </div>
+  );
+}
+
+export default App;
